0cd192e2137b857fe18efd8fc7b32498755fb996
derekreay <derekreay5@gmail.com>
2020-01-31 11:01:34 -0800

    formatted the functions in the same manner and added error handling on inputs, changed output on recent to different dataframe structure and converted date

--- R/datefunc.R
+++ R/datefunc.R
@@ -1,8 +1,8 @@
 library(httr)
 library(anytime)
 
-recall_api <- function(search = NULL, lang = NULL, cat = NULL, lim = NULL) {
-  #handle errors on inputs
+recall_date_api <- function(search = NULL, lang = NULL, cat = NULL, datestart = NULL, dateend = NULL) {
+  #handle errors on input
   if (!is.null(search)) {
     search1 <- paste('search=', search, sep = '')
   }
@@ -18,30 +18,22 @@ recall_api <- function(search = NULL, lang = NULL, cat = NULL, lim = NULL) {
     if (cat %in% c(1,2,3,4)) {
       cat <- paste('&cat=', cat, sep = '')
     } else {
-      warning("Only values of 1,2,3 for cat or category of recall are accepted, searching with cat set to NULL")
+      warning("Only values of 1,2,3 or 4 for cat or category of recall are accepted, searching with cat set to NULL")
       cat <- NULL
     }
-    
   }
-  if (!is.null(lim)) {
-    if (is.numeric(lim)) {
-      lim <- paste('&lim=', lim, sep = '')
-    } else {
-      warning("Lim only accepts integers, With incorrect input will only search for 5")
-      lim <- NULL
-    }
-    
-  }
+ 
   path1 <- '/recall-alert-rappel-avis/api/'
-  path <- paste(path1, 'search?', search1, lang, cat, lim, sep = '')
-  
+  path <- paste(path1, 'search?', search1, lang, cat, '&lim=99999', sep = '')
+  print(path)
   url <- modify_url('https://healthycanadians.gc.ca/recall-alert-rappel-avis/api/', path = path)
   response_recalldetail <- GET(url)
-
+  
   if (http_type(response_recalldetail) != "application/json") {
     stop("API did not return json", call. = FALSE)
   }
-
+  
+  
   if (http_error(response_recalldetail)) {
     stop(
       sprintf(
@@ -52,7 +44,6 @@ recall_api <- function(search = NULL, lang = NULL, cat = NULL, lim = NULL) {
       call. = FALSE
     )
   }
-
   
   get_recalldetail_text <- content(response_recalldetail, as = "text", encoding = "UTF-8")
   
@@ -63,12 +54,27 @@ recall_api <- function(search = NULL, lang = NULL, cat = NULL, lim = NULL) {
   names(df_recalldetail) <- gsub(x = names(df_recalldetail), pattern = ".*\\.", replacement = "")
   #convert datetime object to useful date
   df_recalldetail$date_published <- anytime(df_recalldetail$date_published)
-
+  
+  #date stuff
+  is.POSIXct <- function(x) inherits(x, "POSIXct")
+  
+  datestart <- anytime(datestart)
+  if (is.POSIXct(datestart) == FALSE){
+    cat('datestart entered in incorrect format')
+  }
+  
+  dateend <- anytime(dateend)
+  if (is.POSIXct(dateend) == FALSE){
+    cat('dateend entered in incorrect format')
+  }
+  
+  df_recalldetail<-df_recalldetail[df_recalldetail$date_published > datestart & df_recalldetail$date_published < dateend,]
+  
   #return dataframe without counts column
   return(df_recalldetail[-7])
-
 }
 
-resp <- recall_api(search = 'peanuts', cat = '1', lim = 10, lang = 'en')
-resp
+#resp2 <- recall_date_api(search = 'peanuts', cat = '1', lang = 'en', datestart = '2017-12-19', dateend = '2019-02-07')
+#resp2
+
 
